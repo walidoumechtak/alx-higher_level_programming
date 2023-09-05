67bbcbe0bd767ffe2965cc8452da3fefb2c3bc03
walid oumechtak <woumechtak@gmail.com>
2023-09-05 08:59:33 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+strg = ""
+for i in range(0, 9):
+    for j in range(i, 10):
+        strg = str(i) + str(j)
+        if i == 8 and j == 9 and i != j:
+            print("{}".format(strg))
+        elif i != j:
+            print("{} ,".format(strg), end='')
