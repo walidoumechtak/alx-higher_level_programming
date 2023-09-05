d36aee5342e3e3c70403cdbdcdea2ff09ed21a95
walid oumechtak <woumechtak@gmail.com>
2023-09-05 10:45:06 -0700

    done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        asci = ord(c)
+        asci -= 32
+        newc = chr(asci)
+        print("{}".format(newc), end='')
+    print()
+
+test = "walid"
+uppercase(test)
