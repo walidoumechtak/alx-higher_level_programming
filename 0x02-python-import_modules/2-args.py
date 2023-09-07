050288022808dbae2a5149f79348751a6cda104e
walid oumechtak <woumechtak@gmail.com>
2023-09-07 00:59:11 -0700

    done with exe

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-
-
+import sys
+len_argv = len(sys.argv)
+if len_argv == 1:
+    print("{} arguments.".format(len_argv - 1))
+else:
+    if len_argv == 2:
+        print("{} argument:".format(len_argv - 1))
+    else:
+        print("{} arguments:".format(len_argv - 1))
+for i in range(1, len_argv):
+    print("{}: {}".format(i, sys.argv[i]))
