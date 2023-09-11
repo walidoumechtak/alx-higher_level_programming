3ec1267f376d4977f7e075c773ac936c4514bdce
walid oumechtak <woumechtak@gmail.com>
2023-09-11 05:07:05 -0700

    niely done

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * size_list - function that calculate the size of the linked list
+ * @head: the head of the linked list
+ * Return: the length of the linked list
+ */
+
+int	size_list(listint_t *head)
+{
+	int	i;
+
+	i = 0;
+	while (head != NULL)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
+}
+
+/**
+ * is_palindrome - function that check if a linked list is palindrom
+ * @head: the addr of the head
+ * Return: 1 if it is pali or 0 if its not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t	*half;
+	listint_t	*new;
+	int	len;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	len = size_list(*head) / 2;
+	
+	return (0);
+}
