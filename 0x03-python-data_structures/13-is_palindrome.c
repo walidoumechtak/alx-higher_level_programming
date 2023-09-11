d034d1da37b9d3c06aae725fed93e80ce45cd9a6
walid oumechtak <woumechtak@gmail.com>
2023-09-11 08:16:43 -0700

    done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -20,6 +20,16 @@ int	size_list(listint_t *head)
 }
 
 /**
+ * reverse_list - reverse the linked list
+ * @head: the head of linked list
+ */
+
+void	reverse_list(listint_t **head, )
+{
+
+}
+
+/**
  * is_palindrome - function that check if a linked list is palindrom
  * @head: the addr of the head
  * Return: 1 if it is pali or 0 if its not
@@ -28,19 +38,28 @@ int	size_list(listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t	*half;
-	listint_t	*new;
+	listint_t	*temp;
 	int	len;
+	int	half_len;
 	int	i;
 
 	half = NULL;
+	temp = *head;
 	if (head == NULL || *head == NULL)
 		return (0);
-	len = size_list(*head) / 2;
-	//i = len - (len / 2) + 1;
-	while (i < len)
+	len = size_list(*head);
+	half_len = len / 2;
+	i = 0;
+	while (i++ < half_len)
+		temp = temp->next;
+	if (len % 2 != 0)
+		temp = temp->next;
+	while (temp != NULL)
 	{
-
+		add_nodeint_end(&half, temp->n);
+		temp = temp->next;
 	}
-	
+	reverse_list(&half);
+	free_listint(half);
 	return (0);
 }
