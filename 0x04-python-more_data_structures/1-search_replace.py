33ac2bc99cc737f0ec5651faf73716132783f4dd
walid oumechtak <woumechtak@gmail.com>
2023-09-13 12:38:59 -0700

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,8 @@
 def search_replace(my_list, search, replace):
     newa = []
     for ele in my_list:
-        newe = []
-        for n in range(0, range(0, len(ele))):
-            if ele[n] == search:
-                newe.append(replace)
+            if ele == search:
+                newa.append(replace)
             else:
-                newe.apped(ele[n])
-        newa.append(newe)
+                newa.append(ele)
     return newa
