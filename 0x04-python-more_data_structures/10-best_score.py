0809194c540ddb26a93970967b3d6f4f6af45bce
walid oumechtak <woumechtak@gmail.com>
2023-09-15 03:15:41 -0700

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     max = 0
     retKey = ""
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     obj = a_dictionary.items()
     for key, value in obj:
