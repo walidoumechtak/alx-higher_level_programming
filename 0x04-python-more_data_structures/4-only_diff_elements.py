e3462650d4fefdb924a7c02f5c5bef1d409ad3d1
walid oumechtak <woumechtak@gmail.com>
2023-09-15 00:35:58 -0700

    done

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+def only_diff_elements(set_1, set_2):
+    arr = []
+    for ele in set_1:
+        if ele not in set_2:
+            arr.append(ele)
+    for ele2 in set_2:
+        if ele2 not in set_1:
+            arr.append(ele2)
+    new_set = set(arr)
+    return new_set
