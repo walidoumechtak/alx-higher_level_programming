879baddc5f78e9c815be316ef66cb4a3523cf826
walid oumechtak <woumechtak@gmail.com>
2023-09-15 03:14:02 -0700

    done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    max = 0
+    retKey = ""
+    if a_dictionary == None:
+        return None
+    obj = a_dictionary.items()
+    for key, value in obj:
+        if value > max:
+            max = value
+            retKey = key
+    return retKey
