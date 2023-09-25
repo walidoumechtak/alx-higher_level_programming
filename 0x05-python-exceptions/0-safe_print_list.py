a3f0fb78ed3d535df8ff295315d2a43d1b3472d9
walid oumechtak <woumechtak@gmail.com>
2023-09-25 08:52:45 -0700

    done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        cpt = 0
+        for i in my_list and x > 0:
+            print("{:d}".format(i))
+            x--
+            cpt++
+    except ValueError:
+    return cpt
