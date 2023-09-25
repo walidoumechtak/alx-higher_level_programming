2eea82f72609751e5c5a2e0f1a7dcd8f1f4bf9e5
walid oumechtak <woumechtak@gmail.com>
2023-09-25 12:35:35 -0700

    done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    cpt = 0
     try:
-        cpt = 0
-        for i in my_list and x > 0:
-            print("{:d}".format(i))
-            x--
-            cpt++
+        for i in my_list:
+            if x > 0:
+                print("{:d}".format(i))
+                x = x - 1
+                cpt = cpt + 1
     except ValueError:
         print("error")
     return cpt
