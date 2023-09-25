f65c7c172d36bf5de27f90b075a922e76a7f136f
walid oumechtak <woumechtak@gmail.com>
2023-09-25 12:45:23 -0700

    done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     cpt = False
     try:
-        if isinstance(x, int):
+        if isinstance(value, int):
             print("{:d}".format(value))
             cpt = True
     except ValueError:
