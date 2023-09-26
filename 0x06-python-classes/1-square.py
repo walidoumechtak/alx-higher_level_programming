53da36c1acf59f002ac0a01dfe7c1169cc8a63bf
walid oumechtak <woumechtak@gmail.com>
2023-09-26 02:47:45 -0700

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+"""Define a Square."""
+
+
 class Square:
+    """Represent a square"""
     def __init__(self, size):
-        """ init the class intance """
+        """ init the class intance 
+
+            args:
+                self: the self arg
+                size: the size of the square
+        """
         self.__size = size
