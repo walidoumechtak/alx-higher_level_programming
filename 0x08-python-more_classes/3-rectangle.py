6e8fc761ba0e072dca5ded4a5d41ccf31e4ad627
walid oumechtak <woumechtak@gmail.com>
2023-10-02 06:29:11 -0700

    done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
     def __str__(self):
         """ define how instance should printed """
         rec = ""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return rec
         for i in range(0, self.__height):
             inner = ""
