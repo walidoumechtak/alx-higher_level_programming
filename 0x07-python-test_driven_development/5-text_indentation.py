5b0aaf271ed0485a64020fade2a1e19f99ec7fa1
walid oumechtak <woumechtak@gmail.com>
2023-10-03 03:29:37 -0700

    done

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    contr = 0
+    for i in range(0, len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i], end='')
+            print("")
+            print("")
+            if text[i + 1] == ' ':
+                contr = 1
+
+            else:
+                contr = 0
+        else:
+            if contr == 0:
+                print(text[i], end='')
+            contr = 0
