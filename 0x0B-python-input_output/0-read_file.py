4d705574df0e8364622ce7e08637cc38eccf5e4e
walid oumechtak <woumechtak@gmail.com>
2023-10-10 03:17:55 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+""" define a read_file function"""
+
+
 def read_file(filename=""):
+    """print the content of filename file"""
     with open(filename, 'r', encoding='utf-8') as file:
         cont = file.read()
         print(cont, end='')
