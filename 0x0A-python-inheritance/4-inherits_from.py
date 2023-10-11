d2cf30b6e0ce8e9791060463d829db54f35ff167
walid oumechtak <woumechtak@gmail.com>
2023-10-11 07:10:51 -0700

    done

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define a inherits from function"""
+
+
+def inherits_from(obj, a_class):
+    """ return true if obj is subclass of a_class"""
+    return issubclass(obj, a_class)
