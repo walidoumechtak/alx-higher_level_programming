9e230a36753b45bb3548744a784a39f98dc80b38
walid oumechtak <woumechtak@gmail.com>
2023-10-16 03:16:33 -0700

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,3 +87,8 @@ class Rectangle(Base):
             for j in range(0, self.__width):
                 print("#", end='')
             print("")
+
+    def __str__(self):
+        """override the __str__ method"""
+        strstr = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return strstr
